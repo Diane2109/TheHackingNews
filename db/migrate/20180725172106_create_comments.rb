3a504a1675beb20d7f6d6352afa6e92cbdada445
Diane2109 <40659353+Diane2109@users.noreply.github.com>
2018-07-26 00:59:28 +0200

    Update 20180725172106_create_comments.rb

--- db/migrate/20180725172106_create_comments.rb
+++ db/migrate/20180725172106_create_comments.rb
@@ -1,6 +1,6 @@
 class CreateComments < ActiveRecord::Migration[5.2]
   def change
-    create_table :comments do |t|
+    create_table :comments do |t| # Crée la table des commentaires
       t.belongs_to :link
       t.string :content
       t.timestamps
